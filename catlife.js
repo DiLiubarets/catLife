c220a618804395125453ebe08930cda090ee9e5d
Dina <diana.liubarets@hotmail.com>
2020-05-26 20:39:01 -0400

    css

--- catlife.js
+++ catlife.js
@@ -1,46 +1,42 @@
-
 init();
 
 function init() {
-
-    document.addEventListener('readystatechange', function () {
-        if (document.readyState === "complete") {
-
-            document.getElementById("send").addEventListener("click", convertAgeToCatYears);
-        }
-    });
+  document.addEventListener("readystatechange", function () {
+    if (document.readyState === "complete") {
+      document
+        .getElementById("send")
+        .addEventListener("click", convertAgeToCatYears);
+    }
+  });
 }
-
 
 function convertAgeToCatYears() {
-    var yourAge = document.getElementById("yourAge").value;
-    var year1 = 15;
-    var year2 = 10;
-    var laterYears
-    var text;
-    var myAgeInCatYears
+  var yourAge = document.getElementById("yourAge").value;
+  var year1 = 15;
+  var year2 = 10;
+  var laterYears;
+  var text;
+  var myAgeInCatYears;
 
+  if (isNaN(yourAge) || yourAge < 1 || yourAge > 100) {
+    text = "Input not valid";
+  } else {
+    laterYears = yourAge - 2;
+    laterYears *= 4;
+    myAgeInCatYears = laterYears + year1 + year2;
 
-    if (isNaN(yourAge) || yourAge < 1 || yourAge > 100) {
-
-        text = "Input not valid";
-
-    } else {
-
-        laterYears = yourAge - 2;
-        laterYears *= 4;
-        myAgeInCatYears = laterYears + year1 + year2
-
-        if (yourAge == 1) {
-            myAgeInCatYears = year1;
-        }
-
-        text = myAgeInCatYears;
-
+    if (yourAge == 1) {
+      myAgeInCatYears = year1;
     }
 
-    document.getElementById("p1").innerHTML = ('I am ' + yourAge
-    + ' years old in human which is ' + myAgeInCatYears + ' years old in cat years.');
-    console.log(text)
-}
+    text = myAgeInCatYears;
+  }
 
+  document.getElementById("p1").innerHTML =
+    "I am " +
+    yourAge +
+    " years old in human which is " +
+    myAgeInCatYears +
+    " years old in cat years.";
+  console.log(text);
+}
